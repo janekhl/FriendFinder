6f64f5ecce33b9082c746c0d334e8fd27ee40ff0
Jane <janekhl@yahoo.com>
2018-06-21 21:37:54 -0700

    figured out math stuff I think

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -7,6 +7,30 @@ module.exports = function(app){
     });
 
     app.post("/api/friends", function(req,res){
+        console.log(req.body.scores);
+        var user = req.body;
+        var match = friendsArray[friendIndex];
+        var friendIndex=0; //so the first guy is default match
+        var maxDif = 40; //5-1 *10
+
+        for (var i=0; i<user.scores.length;i++){
+            user.scores[i] = parseInt(user.scores[i]);
+        }
+
+
+        for (var i=0;i<friendsArray.length;i++){
+            var scoreDifference=0;//setting difference low and unatainable so we have something to go off of
+            for (var j = 0; j<friendsArray[i].scores.length; j++){
+                var dif= Math.abs(user.scores[j]-friends[i].scores[j]);
+                scoreDifference+=difference;
+            }
+            if(scoreDifference<maxDif){
+                friendIndex=i;
+                maxDif=scoreDifference;
+            }
+        }
+        res.json(match);
+
     });
 
 }
