642c139d0da5b90ede1bdf66f4cb2a2bd83cf944
Jane <janekhl@yahoo.com>
2018-06-21 20:33:51 -0700

    test

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,164 @@
 var friendsArray = [
-{
-    name:"Ahmed",
-    photo:"www.facebook.com".
-    scores = []
- }
+	{
+		name: 'Blip',
+		photo: 'https://i.pinimg.com/originals/2a/d3/32/2ad3322fe53232d103f6a1ae64e117b8.jpg',
+		scores: [
+			'3',
+			'2',
+			'2',
+			'3',
+			'2',
+			'2',
+			'3',
+			'3',
+			'3',
+			'2'
+		]
+	},
+	{
+		name: 'Bloop',
+		photo: 'https://static.boredpanda.com/blog/wp-content/uploads/2015/02/hot-dudes-reading-books-instagram-12-605x605.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Slop',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSSUCEdMvqTnQISfOyRAZ9pMT6GJj28iTXO7LVNkNprpo1qcCNX9g',
+		scores: [
+			'4',
+			'3',
+			'1',
+			'1',
+			'3',
+			'3',
+			'5',
+			'2',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Schmuck',
+		photo: 'http://www.jeffandwill.com/wp-content/uploads/2015/01/tumblr_ncn4rfhl3n1turm3mo1_1280-e1422629613867.jpg',
+		scores: [
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'1',
+			'1',
+			'1',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Droop',
+		photo: 'http://3.bp.blogspot.com/-MQHUvFyOb9M/TrpdAx-4m9I/AAAAAAAAEaA/yLgiPnGvXrM/s1600/Thorny+darling+I+never+did+expect+you.jpg',
+		scores: [
+			'2',
+			'2',
+			'2',
+			'2',
+			'5',
+			'2',
+			'2',
+			'2',
+			'2',
+			'2'
+		]
+	},
+	{
+		name: 'Drop',
+		photo: 'https://kyoske.files.wordpress.com/2012/04/hot-nerd-reading.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Toot',
+		photo: 'https://i.pinimg.com/736x/90/fe/e5/90fee54cc4f09e181d6a8c57b4e68d70--reading-is-sexy-book-worms.jpg',
+		scores: [
+			'3',
+			'2',
+			'2',
+			'2',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+			'3'
+		]
+	},
+	{
+		name: 'Tot',
+		photo: 'https://thoughtcatalog.files.wordpress.com/2016/04/hotdude.jpg?w=593&h=450&crop=1&resize=593,450&quality=95&strip=all',
+		scores: [
+			'2',
+			'1',
+			'3',
+			'4',
+			'4',
+			'4',
+			'4',
+			'2',
+			'2',
+			'5'
+		]
+	},
+	{
+		name: 'Boop',
+		photo: 'https://lh6.googleusercontent.com/-OqhAs56jnvY/TYmHiszegpI/AAAAAAAAADo/k7NnI7QSy0k/s1600/zacefronreading.jpg',
+		scores: [
+			'5',
+			'5',
+			'2',
+			'5',
+			'1',
+			'5',
+			'3',
+			'3',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Bop',
+		photo: 'http://www.hotandhealthymom.com/wp-content/uploads/2011/07/bomer2.jpg',
+		scores: [
+			'2',
+			'1',
+			'3',
+			'1',
+			'3',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	}
 ];
 
-module.exports = friendsArray
+module.exports = friendsArray;
